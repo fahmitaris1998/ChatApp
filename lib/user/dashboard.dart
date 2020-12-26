f772d28e6f7228029df29871efa7d3bd7e65935c
fahmitaris1998 <fahmitaris1998>
2020-12-26 18:01:21 +0700

    update chatapp

--- lib/user/dashboard.dart
+++ lib/user/dashboard.dart
@@ -48,17 +48,17 @@ class _DashboardState extends State<Dashboard> {
   String user;
   _openPopup(context) {
     Alert(
-        context: context,
-        title: "",
-        content: Column(
-          children: <Widget>[
-            Text(
-              "LOGIN",
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          ],
-        ),
-        ).show();
+      context: context,
+      title: "",
+      content: Column(
+        children: <Widget>[
+          Text(
+            "LOGIN",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ],
+      ),
+    ).show();
   }
 
 
@@ -68,8 +68,8 @@ class _DashboardState extends State<Dashboard> {
     CollectionReference collectionReference = FirebaseFirestore.instance.collection('user');
 //    var username = preferences.getString('username');
     collectionReference
-    .doc(preferences.getString('username'))
-    .update({'tokenandroid':"none"});
+        .doc(preferences.getString('username'))
+        .update({'tokenandroid':"none"});
 
     preferences.remove('username');
     preferences.remove('role');
@@ -84,12 +84,12 @@ class _DashboardState extends State<Dashboard> {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return preferences.getString('username').toString();
   }
-  
+
   Future<void> updatescreen(doc_id) async{
     await Firebase.initializeApp();
     CollectionReference collectionReference = FirebaseFirestore.instance.collection('user');
     collectionReference.doc(doc_id).update({"screen":"dashboard user"});
-    
+
   }
   @override
   void initState() {
@@ -113,6 +113,13 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Colors.blue,
+      //   leading:  IconButton(
+      //       icon: Icon(Icons.menu),
+      //     ),
+      // ),
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
@@ -132,67 +139,23 @@ class _DashboardState extends State<Dashboard> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 35.0),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.menu,
-                              color: Colors.white,
-                              size: 30.0,
+                      SafeArea(
+                          child: Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                              margin: EdgeInsets.only(top: 10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  shape: BoxShape.circle
+                              ),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.menu,
+                                  color: Colors.white,),
+                              ),
                             ),
-                            Expanded(child: Container()),
-                            GestureDetector(
-                              onTap: (){
-                                showDialog(
-                                  context: context,
-                                  child: new AlertDialog(
-                                    content: GestureDetector(
-                                      onTap: (){
-                                        logout(context);
-                                      },
-                                      child: Container(
-                                        padding: EdgeInsets.symmetric(vertical: 10.0),
-                                        decoration: BoxDecoration(
-
-                                        ),
-                                        child: Row(
-                                          children: [
-                                            Icon(Icons.logout),
-                                            SizedBox(width: 10.0,),
-                                            Text("Logout"),
-
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                );
-
-                              },
-                              child: Container(
-                                child: Row(
-                                  children: [
-                                    CircleAvatar(
-                                      backgroundColor: Colors.white,
-                                      radius: 18.0,
-                                      child: Text(user.toString().substring(0,1).toUpperCase(),style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),),
-                                    ),
-                                    SizedBox(width:6.0,),
-                                    Text(user.toString(),
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.bold),
-                                    ),],
-                                ),
-                              )
-                            ),
-
-
-                          ],
-                        ),
+                          )
                       ),
-                      SizedBox(height: 40.0,),
                       Text("Counseling",textAlign: TextAlign.left ,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 30),),
                       SizedBox(height: 30.0,),
                       Text("Are you facing a problem?",textAlign: TextAlign.left ,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
